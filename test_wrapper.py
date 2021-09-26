ed444fcd59de64de06f893718f656dabe5536637
stevegbrooks <stevegbrooks@gmail.com>
2021-09-26 15:49:01 +0800

    more setup

--- test_wrapper.py
+++ test_wrapper.py
@@ -8,14 +8,14 @@ import unittest
 
 class TestInputs(unittest.TestCase):
     
-    def test_get_price():
+    def test_get_price(self):
         pass
 
+class TestOutputs(unittest.TestCase):
+
+    def test_get_price(self):
+        pass
 
 if __name__ == '__main__':
     utils.jq_login("secret.yml")
     unittest.main()
-
-
-
-
