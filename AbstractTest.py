15a1e10b783da0370596ab9da8045069d85324c8
stevegbrooks <stevegbrooks@gmail.com>
2021-09-28 22:17:02 +0800

    added a get_definition function to pull source code from an arbitrary function

--- AbstractTest.py
+++ AbstractTest.py
@@ -1,20 +1,35 @@
 import unittest
 import pandas as pd
+import inspect
+import re
 
 class TestWaffleWrap(unittest.TestCase):
 
     function = None
+
+    def get_definition(self, function):
+        """
+        Get the function definition from the function's source code
+        """
+        source = inspect.getsource(function)
+        #clean up the source code
+        regex = re.compile(r'(?=(def ))([^:]*)') #grab everything between def and :
+        if len(regex.findall(source)[0]) > 0:
+            source = regex.findall(source)[0][1]
+            source = re.sub(r'\s+', ' ', source)
+            source = re.sub(r'\n', ' ', source)
+            source = re.sub(r'\t', ' ', source)
+            return source
+        else:
+            raise ValueError('Function definition not found')
     
     def test_definition(self):
         """
         Test that the function definition aligns with Google Sheet specs
         """
-        #has params called 'tickers' and 'count'
-        self.function(tickers = '000001.XSHE', count = 1)
-        #tickers is first param
-        self.function('000001.XSHE', count = 1)
-        #TODO: add more tests
-        
+        # this test is so variable from function to function, 
+        # that we define it separately each time
+        raise NotImplementedError 
     
     def test_tickers_param(self):
         """
--- run_tests.py
+++ run_tests.py
@@ -2,6 +2,7 @@
 import AbstractTest
 import jqdatasdk as jq
 import utils
+import re
 
 from importlib import reload
 _ = reload(AbstractTest)
@@ -19,6 +20,15 @@ class TestInit(unittest.TestCase):
 
 class TestGetPrice(AbstractTest.TestWaffleWrap): 
     function = jq.get_price
+    def test_definition(self):
+        actual_func_def = self.get_definition(self.function)
+        self.assertEqual(
+            actual_func_def, 
+            "get_price(tickers, start_date=None, end_date=None, \
+                       frequency='daily', fields=None, count=None, \
+                       skip_paused=False, fq='pre', panel=True, \
+                       fill_paused=True)"
+        )
 
 if __name__ == '__main__':
     utils.jq_login("secret.yml")
