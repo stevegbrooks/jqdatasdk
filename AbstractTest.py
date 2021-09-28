3d921325150d142bbeb7d4a78d56be10e00c5662
stevegbrooks <stevegbrooks@gmail.com>
2021-09-28 22:19:51 +0800

    refactored

--- AbstractTest.py
+++ AbstractTest.py
@@ -12,23 +12,24 @@ class TestWaffleWrap(unittest.TestCase):
         Get the function definition from the function's source code
         """
         source = inspect.getsource(function)
-        #clean up the source code
-        regex = re.compile(r'(?=(def ))([^:]*)') #grab everything between def and :
+        #grab everything between def and :
+        regex = re.compile(r'(?=(def ))([^:]*)') 
         if len(regex.findall(source)[0]) > 0:
-            source = regex.findall(source)[0][1]
-            source = re.sub(r'\s+', ' ', source)
-            source = re.sub(r'\n', ' ', source)
-            source = re.sub(r'\t', ' ', source)
-            return source
+            #extract function definition and clean up
+            func_def = regex.findall(source)[0][1]
+            func_def = re.sub(r'\s+', ' ', func_def)
+            func_def = re.sub(r'\n', ' ', func_def)
+            func_def = re.sub(r'\t', ' ', func_def)
+            return func_def
         else:
             raise ValueError('Function definition not found')
     
     def test_definition(self):
         """
         Test that the function definition aligns with Google Sheet specs
+        This test is so variable from function to function, 
+        that we define it separately each time
         """
-        # this test is so variable from function to function, 
-        # that we define it separately each time
         raise NotImplementedError 
     
     def test_tickers_param(self):
